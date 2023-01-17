8f663105ff64b1adc8c7895e85f2ae262cbb2250
John Eskandar <johng.eskandar@gmail.com>
2023-01-17 21:18:36 +0200

    add aeroplane_class_seat_id to create pnr service

--- app/services/pnrs/create_pnr_service.rb
+++ app/services/pnrs/create_pnr_service.rb
@@ -9,7 +9,11 @@ module Pnrs
       # @type [AeroplaneClassSeat] class_seat_config
       @flight.aeroplane_class_seats.each do |class_seat_config|
         class_seats = class_seat_config.row_numbers * class_seat_config.row_seats
-        class_seats.times.each { pnrs << { aeroplane_class_id: class_seat_config.aeroplane_class_id, pnr: 8.times.map { Random.rand(9) }.join } }
+        class_seats.times.each do
+          pnrs << { aeroplane_class_id: class_seat_config.aeroplane_class_id,
+                    aeroplane_class_seat_id: class_seat_config.id,
+                    pnr: 8.times.map { Random.rand(9) }.join }
+        end
       end
       @flight.pnrs.insert_all pnrs
     end
